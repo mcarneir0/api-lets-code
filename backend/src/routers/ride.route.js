2a3de236117dab9c20f7cffab4d61f515a69177e
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-06-22 20:43:14 -0300

    Adicionado Verificação JWT na Criação da Corrida

--- backend/src/routers/ride.route.js
+++ backend/src/routers/ride.route.js
@@ -1,8 +1,9 @@
 const rideController = require('../controllers/rideController');
 const express = require('express');
 const router = express.Router();
+const verifyToken = require('../utils/verifyToken');
 
-router.post('', rideController.createRide);
+router.post('', verifyToken, rideController.createRide);
 router.get('', rideController.getRides);
 router.get('/:userEmail', rideController.getRidesByUserEmail);
 router.get('/:rideId/start', rideController.startRide);
